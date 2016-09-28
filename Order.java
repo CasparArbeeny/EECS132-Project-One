2dfee2cea45703115014f0fa50721543093714ed
CasparArbeeny <cma85@case.edu>
2016-09-28 13:29:12 -0400

    Updating of Order save implemenitng trader

--- Order.java
+++ Order.java
@@ -1,2 +1,57 @@
-public class Order extends Object {
+public class Order extends Trader {
+ 
+  // Stores the symbol of the stock being traded.
+  private final char stockSymbol;
+  
+  // Stores the number of shares in the order.
+  private int shares;
+  
+  // Stores the price of the stock
+  private double price;
+  
+  // Stores whether or not we can only trade all of the shares of the order.
+  private boolean allOrNone;
+  
+  public Order(char stockSymbol, int shares, double price, boolean allOrNone, String trader) {
+   super(trader);
+   this.stockSymbol = stockSymbol;
+   setNumberShares(shares);
+   setPrice(price);
+   setAllOrNone(allOrNone);
+  }
+  
+  // Takes no input and returns the symbol of the stock being traded
+  public char getStockSymbol() {
+    return this.stockSymbol;
+  }
+ 
+  // Takes no input and returns the number of shares being traded
+  public int getNumberShares() {
+    return this.shares;
+  }
+ 
+  // Takes an int and sets the number of shares being traded
+  public void setNumberShares(int shares) {
+    this.shares = shares;
+  }
+  
+  // Takes no input and returnns the price of the stock
+  public double getPrice() {
+    return this.price; 
+  }
+  
+  // Takes a double and sets that as the price of  the stock
+  public void setPrice(double price) {
+    this.price = price;
+  }
+  
+  //  Takes no input and returns if all the sharse a stock neede to be traded
+  public boolean getAllOrNone() {
+    return this.allOrNone;
+  }
+  
+  // Takes a boolean and sets if all the shares of a stock need to be traded
+  public void setAllOrNone(boolean allOrNone) {
+    this.allOrNone = allOrNone;
+  }
 }
