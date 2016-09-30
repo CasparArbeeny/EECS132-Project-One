e5317d53a6ebaa013bfcaa78665297c0eb927b4b
CasparArbeeny <cma85@case.edu>
2016-09-30 13:43:11 -0400

    Beggining of transaction and market classes

--- Market.java
+++ Market.java
@@ -1,2 +1,19 @@
 public class Market extends Trader {
+
+  private char stockSymbol;
+  
+  private double commission;
+  
+  private double tradeFee;
+  
+  private BuyOrder bestBuyOrder;
+  
+  private BuyOrder 2ndBestBuyOrder;
+  
+  private SellOrder bestSellOrder;
+  
+  private SellOrder 2ndBestSellOrder;
+  
+  
+
 }
--- Transaction.java
+++ Transaction.java
@@ -1,2 +1,9 @@
-public class Transaction extends Object {
+public class Transaction extends Order {
+
+  public Transaction(char stockSymbol, int shares, double price, Trader buyer, Trader seller, Market market ) {
+    super(stockSymbol, shares, price, false, null)
+  
+  }
+ 
+
 }
